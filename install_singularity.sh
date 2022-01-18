865f1ea3d88976731ecf9dabc63d6ef43dda7919
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-01-18 22:30:42 +0100

    added singularity installation script

---
+++ install_singularity.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copied from https://github.com/apptainer/singularity/blob/master/INSTALL.md
+
+# Ensure repositories are up-to-date
+sudo apt-get update
+
+# Install debian packages for dependencies
+sudo apt-get install -y \
+    build-essential \
+    libseccomp-dev \
+    pkg-config \
+    squashfs-tools \
+    cryptsetup \
+    curl wget git
+
+# Install Go
+export GOVERSION=1.17.3 OS=linux ARCH=amd64  # change this as you need
+wget -O /tmp/go${GOVERSION}.${OS}-${ARCH}.tar.gz \
+  https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go${GOVERSION}.${OS}-${ARCH}.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
+# Clone the Singularity repo
+git clone https://github.com/hpcng/singularity.git
+cd singularity
+
+# Checkout 3.8
+git checkout v3.8.4
+
+# Compile Singularity
+./mconfig
+cd ./builddir
+make
+sudo make install
+
+# show version
+singularity --version
