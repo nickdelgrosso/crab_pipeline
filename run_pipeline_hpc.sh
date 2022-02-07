289ea98c546c7eee8da34623174a8ab96ba886c7
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-02-07 20:23:33 +0000

    fix rulegraph

--- run_pipeline_hpc.sh
+++ run_pipeline_hpc.sh
@@ -1,2 +1,8 @@
-singularity run --app rulegraph snakemake.sif  > docs/pipeline_graph.svg
+# stop script if an error occurs
+set -e
+
+# Run the pipeline
 singularity run --bind /nfs/winstor/branco/Tiago/Field:/data/raw snakemake.sif
+
+# Render the pipeline to an image (so it's up to date)
+singularity run --bind /nfs/winstor/branco/Tiago/Field/:/data/raw --app rulegraph snakemake.sif > docs/pipeline_dag.svg
